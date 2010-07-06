39b5d78b3eebe6c0c7822b8c7190dffa79c428ca
Jeff McGee <jeff@phodder.com>
2010-07-06 18:08:49 -0500

    made brutal tests

--- tests/query_tests.py
+++ tests/query_tests.py
@@ -46,7 +46,14 @@ class TestQueries(unittest.TestCase):
             (n>3) & (name//'^[tf]') )
         )
     
-    def test_or(self):
+    def test_impossible(self):
+        n = NumberModel.n
+        self.failUnlessEqual( [], _query_to_list(
+            (x==1)&(x>3)))
+        self.failUnlessEqual( [], _query_to_list(
+            (x>3)&(x==1)))
+        
+    def dont_test_or(self):
         n = NumberModel.n
         quad = NumberModel.quad
         name = NumberModel.name
@@ -55,6 +62,17 @@ class TestQueries(unittest.TestCase):
             ((quad>15)&(quad<17)) | ((n>=4)&(n<=5)) ))
         self.failUnlessEqual( [0,3,6,7,10], _query_to_list(
             ((quad>15)|(quad<8)) & ((n<4)|(n>5)) ))
+        
+    def dont_test_nesting(self):
+        n = NumberModel.n
+        quad = NumberModel.quad
+        self.failUnlessEqual( [2,3,5,7], _query_to_list(
+            (((n==2)|(n==3)) |(n==5)) |(n==7) ))
+        self.failUnlessEqual( [2,3,5,7], _query_to_list(
+            (n==2)| ((n==3)| ((n==5)|(n==7))) ))
+        
+        self.failUnlessEqual( [0,3,7,8,9], _query_to_list(
+            (n==0)| ((quad<20)& ((n==3)|(n>6))) ))
 
 if __name__ == '__main__':
     from random import random
