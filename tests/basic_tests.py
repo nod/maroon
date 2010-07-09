636610bedef6d07c9a62430217823c3a17feb641
Jeff McGee <jeff@phodder.com>
2010-07-09 15:54:45 -0500

    fixed stupid bug in test

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -133,7 +133,7 @@ class TestBasicModelCreationAndAssignment(unittest.TestCase):
         item.save()
         result = SimpleModel.find()[0]
         self.failUnlessEqual( result['i2'], None)
-        self.failUnlessEqual( result, item)
+        self.failUnlessEqual( result, item.to_dict())
 
     def test_simple_queries(self):
         self.o1.i1 = 10
