e67086b247df49519f5aba95dee233a05151318f
Jeff McGee <jeff@phodder.com>
2010-10-08 14:36:14 -0500

    removed queries

--- maroon.py
+++ maroon.py
@@ -11,8 +11,6 @@ from copy import copy
 
 SLUG_REGEX = re.compile('[\w@\.]+$')
 
-class BogusQuery(Exception): pass
-
 conf = {
     'database': None,
     'connection': None,
@@ -21,75 +19,6 @@ conf = {
 def connect(host='localhost', port=27017, db_name='maroon'):
     conf['connection'] = pymongo.Connection(host,port)
     conf['database'] = getattr(conf['connection'], db_name)
-
-
-class Q(dict):
-    def __init__(self, d=None):
-        dict.__init__(self,d)
-
-    def __and__(self, v):
-        for key in set(self)&set(v):
-            if key != '$or':
-                raise BogusQuery( "field %s can't match %s and %s"%(
-                        key, str(self[key]), str(v[key])
-                    ))
-        q = Q(self) #we do not want to modify self or v
-        q.update(v)
-        if self.has_key('$or') and v.has_key('$or'):
-            #combine the things in $or using the distributive property
-            #(a|b)&(c|d) -> (a&c | a&d | b&c | b&d)  
-            q['$or'] = [
-                self_term & v_term
-                for self_term in self['$or']
-                for v_term in v['$or']
-            ]
-        return q
-
-    def __or__(self, v):
-        fixed_self = self._to_distributed_list()
-        fixed_v = v._to_distributed_list()
-        return Q({'$or':fixed_self+fixed_v})
-    
-    def _to_distributed_list(self):
-        #returns a list of Q objects that is equivalent to self if the terms
-        #of the list are ORed together
-        if not self.has_key('$or'):
-            return [self]
-        if len(self) ==1:
-            return self['$or']
-        outer = copy(self)
-        del outer['$or']
-        #mongo does not let you nest or statements - use boolean algebra to
-        #return a "sum of products"
-        return [ (outer & inner) for inner in self['$or']]
-
-    def to_mongo_dict(self):
-        d = defaultdict(dict)
-        for key,val in self.iteritems():
-            #crawl the tree
-            if isinstance(val, Q):
-                mongo_value = val.to_mongo_dict()
-            elif hasattr(val, '__iter__') and not isinstance(val, basestring):
-                mongo_value = [
-                        item.to_mongo_dict() if isinstance(item,Q) else item
-                        for item in val
-                ]
-            else:
-                mongo_value = val
-
-            #expand the tuples
-            if isinstance(key, tuple):
-                if key[0] in self:
-                    raise BogusQuery( "field %s can't be %s and match %s"%(
-                            key[0], str(self[key[0]]), str(val)
-                        ))
-                #convert self[('size','$gte')] to d['size']['$gte'] 
-                d[key[0]][key[1]] = mongo_value
-            else:
-                d[key] = mongo_value
-        return d
-
-
 
 
 class Property(object):
@@ -116,16 +45,6 @@ class Property(object):
             return self._to_d(val)
         return val
 
-
-    def __eq__(self, v): return Q({self._name: v})
-    def __ge__(self, v): return Q({(self._name, '$gte'):v})
-    def __gt__(self, v): return Q({(self._name, '$gt' ):v})
-    def __le__(self, v): return Q({(self._name, '$lte'):v})
-    def __lt__(self, v): return Q({(self._name, '$lt' ):v})
-    def __ne__(self, v): return Q({(self._name, '$ne' ):v})
-    
-    def is_in(self, terms): return Q({(self._name, '$in' ):terms})
-    def is_not_in(self, terms): return Q({(self._name, '$nin' ):terms})
 
 class EnumProperty(Property):
     def __init__(self, constants, **kwargs):
@@ -258,8 +177,6 @@ class ListProperty(Property):
             raise ValueError("")
         return val
 
-    def has_all(self, terms): return Q({(self._name, '$all' ):terms})
-
 
 class SlugListProperty(ListProperty):
     def __init__(self, **kwargs):
@@ -345,16 +262,8 @@ class Model(object):
         return getattr(conf['database'],self.__name__)
 
     @classmethod
-    def all(self):
-        return self.collection().find()
-
-    @classmethod
     def find(self, q=None):
-        #print q
-        if q is False or q is True:
-            #make sure we didn't call one of python's comparison operators
-            raise BogusQuery("The first term in a comparison must be a Property.")
-        return self.collection().find(q.to_mongo_dict() if q else None)
+        return self.collection().find(q)
 
     def delete(self):
         if hasattr(self, '_id'):
