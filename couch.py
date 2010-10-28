b0a8a1fe7a21b35eef2510aa1bb683983c055e2e
Jeff McGee <JeffAMcGee@gmail.com>
2010-10-28 14:34:03 -0500

    added paged_view

--- couch.py
+++ couch.py
@@ -1,6 +1,7 @@
 import couchdbkit
 from couchdbkit import Database
 
+
 class CouchDB(Database):
     def save(self, model):
         d = model.to_d()
@@ -19,3 +20,15 @@ class CouchDB(Database):
         for doc in self.all_docs(include_docs=True):
             if doc['doc'].get('doc_type',None) == cls.__name__:
                 yield cls(doc['doc'])
+
+    def paged_view(self, view_name, page_size=1000, **params):
+        #FIXME: you can't set a limit with a paged view!
+        params['limit']=page_size+1
+        while True:
+            res = list(self.view(view_name, **params))
+            for r in res[0:page_size]:
+                yield r
+            if len(res) != page_size+1:
+                break
+            params['startkey']=res[-1]['key']
+            params['startkey_docid']=res[-1]['id']
